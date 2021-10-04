e3c951d5222c561e9f4023cbe74bd7042d5580aa
07Rajat <91456945+07Rajat@users.noreply.github.com>
2021-10-04 19:43:13 +0530

    same value program

---
+++ absolutevalue.java
@@ -0,0 +1,17 @@
+package Atmpin;
+import java.util.Scanner;
+public class absolutevalue {
+
+	public static void main(String[] args) {
+		int a;
+	 Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the Number");
+     System.out.println("Absolute Number");
+     a=sc.nextInt();
+     if(a<0) System.out.println(-1*a);
+     else System.out.println(a);
+    
+    	 
+	}
+
+}
