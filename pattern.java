39ef332f051062ceaa20b2a7c71b77ec06d73ee5
07Rajat <91456945+07Rajat@users.noreply.github.com>
2021-10-04 19:39:17 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,20 @@
+package Atmpin;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		int i,j;
+		char r='A';
+		for(i=1;i<=5;i++)
+		{ r='A';
+		 for(j=1;j<=i;j++)
+		 { 
+			 System.out.print(r++);
+		 }
+			 System.out.println();
+		}
+    	 
+     
+	}
+
+}
